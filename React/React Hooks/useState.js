f5385fc5ace9720962cb32a9dac182f22c7bd853
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-12-29 02:09:54 +0530

    Create useState.js

---
+++ React/React Hooks/useState.js
@@ -0,0 +1,8 @@
+useState is a React Hook used to add state to functional components.
+  
+const [state, setState] = useState(initialValue);
+    state → current value
+    setState → function to update it
+    initialValue → value used on first render only
+Updating state causes re-render
+✔ UI reflects new state automatically
