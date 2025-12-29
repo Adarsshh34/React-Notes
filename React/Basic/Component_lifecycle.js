a05ad387c29af4d970e51e4d24ae65ea742cc008
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2025-12-30 03:24:41 +0530

    Create Component_lifecycle.js

---
+++ React/Basic/Component_lifecycle.js
@@ -0,0 +1,4 @@
+                                                              class base component
+Mounting – component is created and added to the DOM            componentDidMount
+Updating – component re-renders due to changes -> On mount      componentDidUpdate
+Unmounting – component is removed from the DOM  -> Unmounting   componentWillUnmount
