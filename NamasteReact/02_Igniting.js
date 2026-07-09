61541afb20be87cb914b70dd9fd9a4c30dc977fa
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-10 02:40:37 +0530

    Update 02_Igniting.js

--- NamasteReact/02_Igniting.js
+++ NamasteReact/02_Igniting.js
@@ -1,1 +1,24 @@
+npm  - to install packages
+npx - to execute packages
 
+import React from "react" -> "react" is package install in node modules and React comes from there
+
+# Parcel 
+- Dev Build
+- Local Server
+- HMR - hot module replacement (reflect changes in browser just by saving file)
+- File Watching Alogorithm - written in c++
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support old browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod builds
+
+# Browserlist
