26358fcdf547248f8f70bd623bbf5b2ee78c33f8
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-10 02:48:36 +0530

    Update 02_Igniting.js

--- NamasteReact/02_Igniting.js
+++ NamasteReact/02_Igniting.js
@@ -1,7 +1,19 @@
 npm  - to install packages
 npx - to execute packages
 
+# In package.json
+^ -> install minor version like 2.8.3 to 2.8.4
+~ -> install majore version like 2.8 to 3.0
+
+# In Package-lock.json
+maintain exact version of dependency
+while package.json only maintain approximate version
+
 import React from "react" -> "react" is package install in node modules and React comes from there
+
+when we do create-react-app it uses webpacks behind scene
+
+parcel, vite, webpacks -> all are bundlers 
 
 # Parcel 
 - Dev Build
