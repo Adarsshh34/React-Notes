ef3a79ec5f07f812a1ace64177ca50d25e03ebbb
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-13 02:59:58 +0530

    Update 05_lets_get_hooked.js

--- NamasteReact/05_lets_get_hooked.js
+++ NamasteReact/05_lets_get_hooked.js
@@ -1,1 +1,20 @@
+# Export & Import
+ways to export 
+- default export
+  can be used only once
+  we can directly import 
+  import Restaurant from "./Restaurant";
+  
+- named export
+  can be used multiple times
+  to import named export we need to write in curly bracket
+  import { LOGO_URL } from "../Constant";
+
+# Need of using useState
+if we want that when variable changes ui should re-render itself, this is not possible when
+JS let and const that's why we use useState so that it update the state and also triggers re-render
+
+# useState
+
+
 
