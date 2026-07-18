c79bf2b476176fd846c1a55e3c7c1252b28c885f
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-19 02:49:58 +0530

    Update 07_Finding_the_path.js

--- NamasteReact/07_Finding_the_path.js
+++ NamasteReact/07_Finding_the_path.js
@@ -1,1 +1,19 @@
+# useEffect
+1. without dependency array
+useEffect(()=>{
+  console.log("hello");
+})
+console.log is executed after every render
+
+2. With Dependency array empty
+useEffect(()=>{
+  console.log("hello");
+},[])
+console.log is executed only once on intial render
+2. With Dependency array 
+useEffect(()=>{
+  console.log("hello");
+},[var])
+console.log called every time when var changes
+
 
