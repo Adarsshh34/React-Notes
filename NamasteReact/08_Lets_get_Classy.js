7152c18fe7f767a8e89fa203681d209b79b4f975
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-20 03:11:35 +0530

    Update 08_Lets_get_Classy.js

--- NamasteReact/08_Lets_get_Classy.js
+++ NamasteReact/08_Lets_get_Classy.js
@@ -1,1 +1,13 @@
+Class Based Components
 
+* functional Component                                        * Class Based Component
+const User = () => {                                          class UserClass extends React.Component {
+  return (                                                      render() {
+    <div className="user-card">                                      <div className="user-card">
+       <h2>name : Adarsh</h2>                                           <h2>name : Adarsh</h2>
+       <h3>Location : Mumbai</h3>                                        <h3>Location : Mumbai</h3>
+       <h3>Contact : 54687546346</h3>                                    <h3>Contact : 54687546346</h3>
+     </div>                                                           </div>
+  );                                                                 }
+};                                                                  }
+export default User;                                              export default UserClass;
