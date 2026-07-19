147d6e8e738911d91546cd2e46cb552e772198eb
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-20 02:13:12 +0530

    Update 07_Finding_the_path.js

--- NamasteReact/07_Finding_the_path.js
+++ NamasteReact/07_Finding_the_path.js
@@ -17,4 +17,47 @@ useEffect(()=>{
 console.log called every time when var changes
 
 # React Router Dom
+import "./styles.css";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+import Error from "./Components/Error";
+import About from "./Components/About";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+export default function App() {
+  const AppLayout = () => {
+    return (
+      <div className="App">
+        <Header />
+        <Outlet />
+      </div>
+    );
+  };
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [                            ----> Children Routes: as per the route changes <Outlet/> is replaced with 
+        {                                          elements   
+          path: "/",
+          element: <Body />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={appRouter} />;
+}
 
+# Linking a url
+import { Link } from "react-router-dom";
+<li>
+    <Link to="/">Home</Link>
+</li>
+
+# Single Page Application
+Why react is called single page application -> 
+  when routes changes website doesn't reload because 
+  React does not reload instead React just replaces Component
