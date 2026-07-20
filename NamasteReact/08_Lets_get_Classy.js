ccc45669fe79fd81dcc6a9d011853b90770395fb
Adarsh Kamble <102070546+Adarsshh34@users.noreply.github.com>
2026-07-21 02:39:14 +0530

    Update 08_Lets_get_Classy.js

--- NamasteReact/08_Lets_get_Classy.js
+++ NamasteReact/08_Lets_get_Classy.js
@@ -11,3 +11,26 @@ const User = () => {                                          class UserClass ex
   );                                                                 }
 };                                                                  }
 export default User;                                              export default UserClass;
+
+
+
+
+#Life Cycle of Class Based Component
+- sequence of execution in class based
+  Constructor()
+  render()
+  ComponentDidMount()
+
+= Interview Question : if there are 
+  Parent constructor,Parent render, Child ComponentDidMount
+  Child constructor,Child render, Child ComponentDidMount
+what will be the sequence of execution
+
+Parent Constructor
+Parent Render
+Child Constructor
+Child Render
+Child ComponentDidMount
+Parent ComponentDidMount
+
+
